0019aefb4c62b3dc92d4a93e2fe27353baee78db
PrakashMakwana007 <pasatvara7777@gmail.com>
2025-09-12 16:51:28 +0530

    fix

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // Replace with your backend URL
-const BASE_URL = 'http://localhost:5000/api/v1'
+const BASE_URL = import.meta.env.VITE_API_BASE
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
